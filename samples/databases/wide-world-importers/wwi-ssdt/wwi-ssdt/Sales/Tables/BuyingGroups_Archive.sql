4ed4ce553996ef62501027afcdd2c5e660c77689
nathan.heaivilin <heaivilin@gmail.com>
2022-03-23 22:52:46 -0500

    fix crap

--- samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Sales/Tables/BuyingGroups_Archive.sql
+++ samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Sales/Tables/BuyingGroups_Archive.sql
@@ -3,8 +3,7 @@
     [BuyingGroupName] NVARCHAR (50) NOT NULL,
     [LastEditedBy]    INT           NOT NULL,
     [ValidFrom]       DATETIME2 (7) NOT NULL,
-    [ValidTo]         DATETIME2 (7) NOT NULL,
-    Nathan  bit default 0
+    [ValidTo]         DATETIME2 (7) NOT NULL
 );
 
 
