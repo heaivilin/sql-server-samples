f43c445377d93e6f346f59bbe49048722a5d54af
nathan.heaivilin <heaivilin@gmail.com>
2022-09-18 13:06:06 -0500

    auto build test

--- samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Security/Application.sql
+++ samples/databases/wide-world-importers/wwi-ssdt/wwi-ssdt/Security/Application.sql
@@ -5,5 +5,5 @@
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'Description', @value = N'Tables common across the application. Used for categorization and lookup lists, system parameters and people (users and contacts)', @level0type = N'SCHEMA', @level0name = N'Application';
+--EXECUTE sp_addextendedproperty @name = N'Description', @value = N'Tables common across the application. Used for categorization and lookup lists, system parameters and people (users and contacts)', @level0type = N'SCHEMA', @level0name = N'Application';
 
